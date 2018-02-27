068014aea3612d0c65645f032857f62dbb44da28
alexsbygaga <alexsbygaga@gmail.com>
2018-02-27 08:56:52 +0300

    src/index.js add code with salvation of 4th exercise.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var arr = equation.split(' ');
+    var first = Number(arr[0]);
+    var second = Number(arr[3] + arr[4]);
+    var third = Number(arr[7] + arr[8]);
+    var D = Number(second * second - 4 * first * third);
+    var sol1 = Math.round((-1 * second + Math.sqrt(D))/(2 * first));
+    var sol2 = Math.round((-1 * second - Math.sqrt(D))/(2 * first));
+    var solutions = [];
+    solutions.push(sol1);
+    solutions.push(sol2);
+    solutions.sort(function(a, b) {
+        return a - b;
+    });
+    return solutions;
 }
